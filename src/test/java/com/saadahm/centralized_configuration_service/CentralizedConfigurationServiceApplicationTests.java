2ef5d06ef615b6b107706b6eb279e81714d03fee
Saad Ahmad <saadtahir5@gmail.com>
2024-12-08 20:19:17 -0500

    mvp config service

---
+++ src/test/java/com/saadahm/centralized_configuration_service/CentralizedConfigurationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saadahm.centralized_configuration_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralizedConfigurationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
